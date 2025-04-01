e1e34b31b064232e79314c24ef074a5da233b8dd
AndrewwBC <andrew-hot2011@hotmail.com>
2025-04-01 01:10:18 -0300

    initial commit

---
+++ categories/src/test/java/com/filo/categories/CategoriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filo.categories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CategoriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
