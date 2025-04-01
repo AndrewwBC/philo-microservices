7722772b46dd77b59e1cb525a06b549d73ce0f02
AndrewwBC <andrew-hot2011@hotmail.com>
2025-04-01 20:10:57 -0300

    feat(users): user service

---
+++ users/src/main/java/com/filo/users/service/UserService.java
@@ -0,0 +1,16 @@
+package com.filo.users.service;
+
+
+import com.filo.users.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+}
