44faeb9b3bc37ff41adc8da89d4a8a7c6af0ec1c
AndrewwBC <andrew-hot2011@hotmail.com>
2025-04-03 22:57:03 -0300

    add(users): find user or throw method

--- users/src/main/java/com/filo/users/service/UserService.java
+++ users/src/main/java/com/filo/users/service/UserService.java
@@ -26,16 +26,11 @@ public class UserService {
         String cryptPass = new BCryptPasswordEncoder().encode(userDTO.password());
         UserModel user = UserModel.fromDataCreateUser(userDTO, cryptPass, userRole);
         return this.userRepository.save(user);
-
-        //return new ResponseDTO(user.getId(), user.getUsername());
     }
 
     public UserModel updateUser(UserDTO userDTO, String id) {
         UserRole userRole = this.handleUserRole(userDTO.email());
-
-        UserModel user = this.userRepository.findById(id).orElseThrow(() ->
-                new NoSuchElementException("Usuário não encontrado"));
-
+        UserModel user = this.findUserOrThrow(id);
         user.updateFromUserDTO(userDTO, userRole);
         return this.userRepository.save(user);
     }
@@ -45,5 +40,9 @@ public class UserService {
         return userRole;
     }
 
+    private UserModel findUserOrThrow(String id) {
+        return this.userRepository.findById(id).orElseThrow(() ->
+                new NoSuchElementException("User not found"));
+    }
 
 }
