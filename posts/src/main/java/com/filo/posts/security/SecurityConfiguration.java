cf3a656e10a62b166cc50a419a0ac47e053082b6
AndrewwBC <andrew-hot2011@hotmail.com>
2025-04-03 23:16:56 -0300

    feat(posts): security config

---
+++ posts/src/main/java/com/filo/posts/security/SecurityConfiguration.java
@@ -0,0 +1,21 @@
+package com.filo.posts.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.POST, "/api/v1/posts").permitAll()).build();
+    }
+}
